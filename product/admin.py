c31765f551fbdec0076ccfa2ca96ee11202571a6
MohamadAlkhoja <muhummad.kh@gmail.com>
2021-05-07 22:39:02 +0300

    Admin Gallery

--- product/admin.py
+++ product/admin.py
@@ -1,16 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category, Product
+from product.models import Category, Product, Images
+
+
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'price', 'status']
     list_filter = ['status', 'category']
+    inlines = [ProductImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product', 'image']
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Images, ImagesAdmin)
