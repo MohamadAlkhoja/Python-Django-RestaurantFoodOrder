a3fd960d47bdd11451eea5714a79a7a49a2fc800
MohamadAlkhoja <muhummad.kh@gmail.com>
2021-06-22 02:37:07 +0300

    Make Order

--- order/admin.py
+++ order/admin.py
@@ -1,11 +1,34 @@
 from django.contrib import admin
 
 # Register your models here.
-from order.models import ShopCart
+from order.models import ShopCart, OrderProduct, Order
 
 
 class ShopCartAdmin(admin.ModelAdmin):
     list_display = ['user', 'product', 'price', 'quantity', 'amount']
     list_filter = ['user']
 
+
+class OrderProductline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('user', 'product', 'price', 'quantity', 'amount')
+    can_delete = False
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['code', 'tableno', 'total', 'status']
+    list_filter = ['status']
+    readonly_fields = ('user', 'ip', 'total')
+    can_delete = False
+    inlines = [OrderProductline]
+
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'price', 'quantity', 'amount']
+    list_filter = ['user']
+
+
 admin.site.register(ShopCart, ShopCartAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProduct, OrderProductAdmin)
--- order/models.py
+++ order/models.py
@@ -28,3 +28,55 @@ class ShopCartForm(ModelForm):
     class Meta:
         model = ShopCart
         fields = ['quantity']
+
+
+class Order(models.Model):
+    STATUS= (
+        ('New', 'New'),
+        ('Accepted', 'Accepted'),
+        ('Preaparing', 'Preaparing'),
+        ('OnTable', 'OnTable'),
+        ('Canceled', 'Canceled'),
+    )
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    code = models.CharField(max_length=5)
+    tableno = models.CharField(max_length=20, blank=True)
+    total = models.CharField(max_length=15, blank=True)
+    status = models.CharField(max_length=15, default='New', choices=STATUS)
+    ip = models.CharField(max_length=20, blank=True)
+    adminnote = models.CharField(max_length=100, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.first_name
+
+
+class OrderForm(ModelForm):
+
+    class Meta:
+        model = Order
+        fields = ['tableno']
+
+
+class OrderProduct(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Accepted', 'Accepted'),
+        ('Canceled', 'Canceled'),
+    )
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    amount = models.FloatField()
+    total = models.CharField(max_length=15, blank=True)
+    status = models.CharField(max_length=15, default='New', choices=STATUS)
+    ip = models.CharField(max_length=20, blank=True)
+    adminnote = models.CharField(max_length=100, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.title
